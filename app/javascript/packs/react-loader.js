70b1202161bf61723e15bdb1327b10d5a329f988
sherwin-kwan <sherwin.kwan92@gmail.com>
2021-04-29 23:58:11 -0700

    remove react

--- app/javascript/packs/react-loader.js
+++
@@ -1,8 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-
-import { Hello } from '../components/hello_react';
-
-// This is how react_on_rails can see the HelloWorld in the browser.
-ReactOnRails.register({
-  Hello,
-});
