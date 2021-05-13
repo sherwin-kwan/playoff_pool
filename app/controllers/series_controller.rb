89a0beb5e4767c80421ce3e79dc40cf55ab2aa83
sherwin-kwan <sherwin.kwan92@gmail.com>
2021-05-12 21:07:13 -0700

    add interface to edit series

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,12 +1,12 @@
 class SeriesController < ApplicationController
+
+  before_action :check_privileges, only: [:edit, :new]
+
   def index
     @series = Series.all.filter(&:active?)
   end
 
   def new
-    unless User.find(session[:current_user]).admin?
-      render "application/error", {locals: {message: "You do not have permission to view this page"}}
-    end
     @this_series = Series.new 
   end
 
@@ -28,7 +28,12 @@ class SeriesController < ApplicationController
   end
 
   def update
-    puts "Not implemented yet, wait a minute please"
+    @this_series = Series.find(params[:id])
+    if @this_series.update(series_params)
+      redirect_to series_index_path
+    else
+      redirect_to edit_series_path, flash: {errors: @this_series.errors.full_messages}
+    end
   end
 
   def destroy
@@ -39,5 +44,11 @@ class SeriesController < ApplicationController
 
   def series_params
     params.require(:series).permit(:team1_id, :team2_id, :round_id, :start_time)
+  end
+
+  def check_privileges
+    unless User.find(session[:current_user]).admin?
+      render "application/error", {locals: {message: "You do not have permission to view this page"}} and return
+    end
   end
 end
