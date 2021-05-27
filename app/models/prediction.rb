ce9bc4e5a6b8c14f59a6f0d0863c7c533943a3ec
sherwin-kwan <sherwin.kwan92@gmail.com>
2021-05-26 21:49:22 -0700

    restore active? to run rake task

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -32,6 +32,12 @@ class Prediction < ApplicationRecord
     "#{self.winner.short_name} in #{self.games}"
   end
 
+  def active?
+    return false unless self.series.active?
+    newer_predictions = Prediction.where(series_id: self.series_id).where(user_id: self.user_id).where("created_at > ?", self.created_at)
+    return newer_predictions.size > 0 ? false : true
+  end
+
   private
   def predict_before_series 
     if self.series.start_time && self.series.start_time < Time.now
