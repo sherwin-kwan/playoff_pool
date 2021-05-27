0f485b67098af07d8027dd813423e3ff2a0678e3
sherwin-kwan <sherwin.kwan92@gmail.com>
2021-05-26 21:41:55 -0700

    implement performance improvements

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -3,7 +3,8 @@ class RoundsController < ApplicationController
   def show
     @rounds = Round.all.select([:id, :fancy_name])
     @round = Round.find(params[:id])
-    @predictions = Prediction.all.includes(:winner).filter{|pred| pred.series.round.id.to_s == params[:id]}
+    @series = @round.series.includes(:team1).includes(:team2).includes(:winner)
+    @predictions = @series.map{|ser| ser.predictions.includes(:winner)}.flatten
     @users_with_picks = User.find(self.users_with_picks.map{|h| h["id"]})
   end
 
