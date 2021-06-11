5981586305267365490f663a641cb25cf735b576
sherwin-kwan <sherwin.kwan92@gmail.com>
2021-06-10 21:29:17 -0700

    set up current round link properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
 
+  helper_method :current_round
+
+  def current_round
+    2
+  end
+
   def current_user
     User.find(session[:current_user])
   end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,5 @@ class MainController < ApplicationController
 
   def index 
     @users = User.all.filter(&:has_picks?).sort_by(&:rank)
-    @CURRENT_ROUND = 2
   end
 end
