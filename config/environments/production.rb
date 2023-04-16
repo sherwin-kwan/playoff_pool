4efba952498c96d358f549eb10949586486be323
sherwin-kwan <sherwin.kwan92@gmail.com>
2023-04-15 21:55:58 -0700

    update production db credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,9 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
