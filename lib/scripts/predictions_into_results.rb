c50dc9216bcd54376ac7efc57c3b27bc0ea539bc
Sherwin Kwan <sherwin.kwan92@gmail.com>
2026-04-17 00:55:57 -0700

    fix script - results shouldn't include current year, also make the script idempotent

--- lib/scripts/predictions_into_results.rb
+++ lib/scripts/predictions_into_results.rb
@@ -1,10 +1,11 @@
 first_year_in_db = Prediction.joins(:series).pluck(:year).min
-years_to_run_script_on = (first_year_in_db..Time.now.year).to_a
+years_to_run_script_on = (first_year_in_db...Time.now.year).to_a
 
 years_to_run_script_on.each do |year|
   predictions_in_year = Prediction.joins(:series).where(series: {year: year})
   users_in_year = predictions_in_year.pluck(:user_id).uniq
   users_in_year.each do |user_id|
+    next if Result.find_by(user_id: user_id, year: year).present?
     users_predictions = predictions_in_year.where(user_id: user_id)
     correct = users_predictions.joins(:series).where("series.winner_id = predictions.winner_id")
     correct_count = correct.count
